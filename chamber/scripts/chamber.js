dad3b16c2ceaee2892dc43ce39d696829db4fd45
Samuel Chacon <sechafer@hotmail.com>
2024-05-11 18:13:57 -0300

    fix error week2

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,27 +1,28 @@
 
-// Display the copyright year
 function getCopyrightYear() {
-    const year = new Date().getFullYear();
-    document.getElementById("cYear").innerHTML = `&copy; ${year}`;
-}
+        const year = new Date().getFullYear();
+        return `© ${year}`;
+    }
+    document.getElementById("cYear").innerHTML = getCopyrightYear();
 
-// Display the last modified date of the page
-function getLastModified() {
-    const lastModified = new Date(document.lastModified).toGMTString();
-    document.getElementById("lastModified").innerHTML = `Last Modified: ${lastModified}`;
-}
+    // modificacion e impresion de fecha
+    function getLastModified() {
+        const lastModified = new Date(document.lastModified).toGMTString();
+        return `Last Modified:  ${lastModified}`;
+    }
+    document.getElementById("lastModified").innerHTML = getLastModified();
 
-// Toggle Dark/Light Mode
-function screenMode() {
-    const element = document.body;
-    element.classList.toggle("dark-mode");
+    // Toggle Dark/Light Mode
+    function screenMode() {
+        const element = document.body;
+        element.classList.toggle("dark-mode");
 
-    const sections = document.querySelectorAll(
-        "main, div.info, div.spotlights, div.event, div.event-card, section.style5, article.card, div.weather-info, div.form-wrapper, section.member, section.member a, div.form-wrapper, form label.top input, form label.sbs, form fieldset, form legend, form label.top"
-    );
-    sections.forEach((section) => {
-        section.classList.toggle("dark-mode");
-    });
-}
+        const sections = document.querySelectorAll(
+            "main, div.info, div.spotlights, div.event, div.event-card, section.style5, article.card, div.weather-info, div.form-wrapper, section.member, section.member a, div.form-wrapper, form label.top input, form label.sbs, form fieldset, form legend, form label.top"
+        );
+        sections.forEach((section) => {
+            section.classList.toggle("dark-mode");
+        });
+    }
 
 
