7881cddc74acf21b7821c3e55c9b5eeab172bfae
Samuel Chacon <sechafer@hotmail.com>
2024-05-29 04:41:06 -0300

    fix banner

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -2,19 +2,26 @@
 function initBanner() {
     var banner = document.getElementById("banner");
     var closeBannerBtn = document.getElementById("closeBanner");
-  
+ 
     // Check if today is Monday, Tuesday, or Wednesday
     var today = new Date();
     var dayOfWeek = today.getDay();
-    // var dayOfWeek = 1; // for testing purposes
     var isBannerVisible = dayOfWeek >= 1 && dayOfWeek <= 3;
-  
+ 
+    // Check session storage to see if banner has been closed
+    var isBannerClosed = sessionStorage.getItem("bannerClosed") === "true";
+ 
     // Set initial visibility
-    banner.classList.toggle("banner", isBannerVisible);
-    banner.classList.toggle("banner-hide", !isBannerVisible);
-  
+    banner.classList.toggle("banner", isBannerVisible && !isBannerClosed);
+    banner.classList.toggle("banner-hide", !isBannerVisible || isBannerClosed);
+ 
     // Close the banner when the close button is clicked
     closeBannerBtn.addEventListener("click", function () {
         banner.classList.add("banner-hide");
+        // Store the banner closed state in session storage
+        sessionStorage.setItem("bannerClosed", "true");
     });
-  }
+}
+
+// Initialize the banner when the document is fully loaded
+document.addEventListener("DOMContentLoaded", initBanner);
